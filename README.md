aeed94b729777311b1a8f582e74caa196b6b59c7
Diane Douglas, Lisa MacCarrigan and Meredith Alcorn <diane+lisa+meredith@>
2017-01-28 19:40:31 -0500

    initial starter kit.

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# Repository contents
+
+This repository contains the full application of the course Angular and Firebase - Build a Web Application, so this contains the complete application like it will look like at the end of the course. 
+
+![Angular firebase course](https://angular-academy.s3.amazonaws.com/thumbnails/angular_app-firebase-small.jpg)
+
+
+# Looking for the full application repo ? 
+
+If you are looking for a repository containing the full application that is built during the course, please check this [repository](https://github.com/angular-university/angular-firebase-app) instead.
+
+
+# Installation pre-requisites
+
+For running this project we need and npm installed on our machine. These are some tutorials to install node in different operating systems: 
+
+*Its important to install the latest version of Node*
+
+- [Install Node and NPM on Windows](https://www.youtube.com/watch?v=8ODS6RM6x7g)
+- [Install Node and NPM on Linux](https://www.youtube.com/watch?v=yUdHk-Dk_BY)
+- [Install Node and NPM on Mac](https://www.youtube.com/watch?v=Imj8PgG3bZU)
+
+
+# Installing the Angular CLI
+
+With the following command the angular-cli will be installed globally in your machine:
+
+    npm install -g angular-cli
+
+# Installing the code
+
+The code can be installed with the following command (needs to be run in the folder where package.json is):
+
+    npm install 
+
+
+## Running the code
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
---
+++ populate-db.ts
@@ -0,0 +1,71 @@
+import {database, initializeApp} from "firebase";
+import {firebaseConfig} from "./src/environments/firebase.config";
+import {dbData} from "./db-data";
+
+
+console.log("WARNING VERY IMPORTANT - PLEASE READ THIS\n\n\n"); 
+console.log('WARNING Please set your own firebase config on src/environmnents/firebase.config.ts');
+console.log('Otherwise you will get permissions errors, because the populate-db script is trying to write to my database instead of yours. ');
+console.log('Any issues please contact me, Thanks, Vasco\n\n\n');
+
+initializeApp(firebaseConfig);
+
+
+const coursesRef = database().ref('courses');
+const lessonsRef = database().ref('lessons');
+
+
+
+dbData.courses.forEach( course => {
+
+  console.log('adding course', course.url);
+
+  const courseRef = coursesRef.push({
+      url: course.url,
+      description: course.description,
+      iconUrl: course.iconUrl,
+      courseListIcon: course.courseListIcon,
+      longDescription: course.longDescription
+  });
+
+  let lessonKeysPerCourse = [];
+
+  course.lessons.forEach((lesson:any) =>  {
+
+    console.log('adding lesson ', lesson.url);
+
+    lessonKeysPerCourse.push(lessonsRef.push({
+        description: lesson.description,
+        duration: lesson.duration,
+        url: lesson.url,
+        tags: lesson.tags,
+        videoUrl: lesson.videoUrl || null,
+        longDescription: lesson.longDescription,
+        courseId: courseRef.key
+      }).key);
+
+  });
+
+
+  const association = database().ref('lessonsPerCourse');
+
+  const lessonsPerCourse = association.child(courseRef.key);
+
+  lessonKeysPerCourse.forEach(lessonKey => {
+    console.log('adding lesson to course ');
+
+    const lessonCourseAssociation = lessonsPerCourse.child(lessonKey);
+
+    lessonCourseAssociation.set(true);
+  }); 
+
+
+});
+
+
+
+
+
+
+
+
