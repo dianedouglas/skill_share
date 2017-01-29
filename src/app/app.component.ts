ecbee73b81a53c91bb409bb8d41bec44b6e7e13c
Diane Douglas, Lisa MacCarrigan and Meredith Alcorn <diane+lisa+meredith@>
2017-01-28 20:25:53 -0500

    move initial printing over to app component and add firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database} from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(){
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDhyRz5WogFqRN9wBYLLIVhFUIOuNeebts",
+      authDomain: "skill-share-angular2-19e00.firebaseapp.com",
+      databaseURL: "https://skill-share-angular2-19e00.firebaseio.com",
+      storageBucket: "skill-share-angular2-19e00.appspot.com",
+      messagingSenderId: "729612898539"
+    };
+    initializeApp(config);
+    var root = database().ref();
+    root.on('value', function(snap){
+      console.log(snap.val());
+    })
+  }
 }
