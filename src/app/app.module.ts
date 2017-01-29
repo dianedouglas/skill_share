b18b313d0aa52419f1d3c237f0b8fd556729ed44
Diane Douglas, Lisa MacCarrigan and Meredith Alcorn <diane+lisa+meredith@>
2017-01-28 23:07:19 -0500

    add register and login components.  add form for login using FormBuilder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { firebaseConfig } from "../environments/firebase.config";
 import { AngularFireModule } from "angularfire2/index";
 import { SkillsService } from "./shared/model/skills.service";
+import { Skill } from "./shared/model/skill";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import 'rxjs/add/operator/map';
@@ -15,6 +17,7 @@ import { routerConfig } from './router.config';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthService } from './shared/security/auth.service';
 
 @NgModule({
   declarations: [
@@ -28,11 +31,12 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AngularFireModule.initializeApp(firebaseConfig),
     RouterModule.forRoot(routerConfig)
   ],
-  providers: [SkillsService],
+  providers: [SkillsService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb:FormBuilder) { 
+    this.form = this.fb.group({
+      email: ['Enter Email', Validators.required],
+      password: ['Enter Password', Validators.required],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  login(){
+    
   }
 
 }
